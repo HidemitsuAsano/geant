c430520d49925acc0265e877e495fb534d9935ae
ASANO Hidemitsu <hidemitsu.asano@riken.jp>
2020-09-25 11:13:23 +0900

    update

--- knucl4.10/src/KnuclPrimaryGeneratorAction.cc
+++ knucl4.10/src/KnuclPrimaryGeneratorAction.cc
@@ -920,12 +920,41 @@ int KnuclPrimaryGeneratorAction::KminusReac(G4Event* anEvent, const CrossSection
   } // if( anaManager->GetFowardAccept() ){
   //std::cerr<<"num of loop = "<<counter<<std::endl;
   
-  if(MakeUniformInqmass){
+  if(MakeUniformInqmass==1){
     //---------------------------------------------------//
     //uniform distribution for 3-body 
     //---------------------------------------------------//
     G4LorentzVector lvec[3];
     for(int i=0;i<3;i++){
+      lvec[i].setVectM(vec[i], mass[i]);//vec is 3-mom. vec in CM frame.
+      lvec[i].boost(boost);//boost to the lab frame
+    }
+    //0: missing neutron
+    //1: S+/-
+    //2: pi-/+
+    G4LorentzVector TL_piSigma = lvec[1]+lvec[2];
+
+    //npipiL simulation
+    //0: n
+    //1: pi+
+    //2: pi-
+    //3: missing Lambda
+    //G4LorentzVector TL_piSigma = lvec[0]+lvec[1]+lvec[2];
+    G4ThreeVector beammom(0,0,1000.);
+    G4LorentzVector TL_beam;
+    TL_beam.setVectM(beammom,493.);
+    //double q = (TL_beam.vect()-lvec[3].vect()).mag()/1000.;//npipiL
+    double q = (TL_beam.vect()-lvec[0].vect()).mag()/1000.;//piSigma
+    double piSmass = TL_piSigma.m()/1000.;
+
+    double prob = h2genprob->Interpolate(piSmass,q);
+    if(  prob <  G4UniformRand()) goto START;
+  }
+  
+  //npipiL sim.
+  if(MakeUniformInqmass==2){
+    G4LorentzVector lvec[4];
+    for(int i=0;i<4;i++){
       lvec[i].setVectM(vec[i], mass[i]);//vec is 3-mom. vec in CM frame.
       lvec[i].boost(boost);//boost to the lab frame
     }
